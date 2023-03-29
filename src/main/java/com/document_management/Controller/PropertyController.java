a37bc5b24be310e9d2e1c7181e012b2b5c639b27
divya wani <divyaw@bbd.co.za>
2023-03-29 14:37:22 +0530

    commit changes

--- src/main/java/com/document_management/Controller/PropertyController.java
+++ src/main/java/com/document_management/Controller/PropertyController.java
@@ -1,42 +1,41 @@
-//package com.hello.hello.Controller;
-//        import java.util.List;
-//
-//        import com.hello.hello.Entity.Property;
-//        import com.hello.hello.Service.PropertyService;
-//        import org.springframework.beans.factory.annotation.Autowired;
-//        import org.springframework.http.HttpStatus;
-//        import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//@RequestMapping("/api/properties")
-//public class PropertyController {
-//    @Autowired
-//    private PropertyService propertyService;
-//
-//    @GetMapping("/get")
-//    public List<Property> getAllProperties() {
-//        return propertyService.getAllProperties();
-//    }
-//
-//    @GetMapping("/{id}")
-//    public Property getPropertyById(@PathVariable int id) {
-//        return propertyService.getPropertyById(id);
-//    }
-//
-//    @PostMapping("/add")
-//    @ResponseStatus(HttpStatus.CREATED)
-//    public Property createProperty(@RequestBody Property property) {
-//        return propertyService.createProperty(property);
-//    }
-//
-//    @PutMapping("/{id}")
-//    public Property updateProperty(@PathVariable int id, @RequestBody Property propertyDetails) {
-//        return propertyService.updateProperty(id, propertyDetails);
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    @ResponseStatus(HttpStatus.NO_CONTENT)
-//    public void deleteProperty(@PathVariable int id) {
-//        propertyService.deleteProperty(id);
-//    }
-//}
+package com.document_management.Controller;
+        import com.document_management.Entity.Property;
+        import com.document_management.Service.PropertyService;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.http.HttpStatus;
+        import org.springframework.web.bind.annotation.*;
+        import java.util.List;
+
+@RestController
+@RequestMapping("/api/properties")
+public class PropertyController {
+    @Autowired
+    private PropertyService propertyService;
+
+    @GetMapping("/get")
+    public List<Property> getAllProperties() {
+        return propertyService.getAllProperties();
+    }
+
+    @GetMapping("/{id}")
+    public Property getPropertyById(@PathVariable int id) {
+        return propertyService.getPropertyById(id);
+    }
+
+    @PostMapping("/add")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Property createProperty(@RequestBody Property property) {
+        return propertyService.createProperty(property);
+    }
+
+    @PutMapping("/{id}")
+    public Property updateProperty(@PathVariable int id, @RequestBody Property propertyDetails) {
+        return propertyService.updateProperty(id, propertyDetails);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteProperty(@PathVariable int id) {
+        propertyService.deleteProperty(id);
+    }
+}
--- src/main/java/com/document_management/Service/PropertyService.java
+++ src/main/java/com/document_management/Service/PropertyService.java
@@ -1,53 +1,53 @@
-//package com.hello.hello.Service;
-//        import java.util.List;
-//
-//        import com.hello.hello.Entity.Property;
-//        import com.hello.hello.Repository.PropertyRepository;
-//        import org.springframework.beans.factory.annotation.Autowired;
-//        import org.springframework.http.HttpStatus;
-//        import org.springframework.stereotype.Service;
-//        import org.springframework.web.bind.annotation.ResponseStatus;
-//
-//@Service
-//public class PropertyService {
-//    @Autowired
-//    private PropertyRepository propertyRepository;
-//
-//    public List<Property> getAllProperties() {
-//        return propertyRepository.findAll();
-//    }
-//
-//    public Property getPropertyById(int id) {
-//        return propertyRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Property", "id", id));
-//    }
-//
-//    public Property createProperty(Property property) {
-//        return propertyRepository.save(property);
-//    }
-//
-//    public Property updateProperty(int id, Property propertyDetails) {
-//        Property property = getPropertyById(id);
-//
-//        property.setPropertyName(propertyDetails.getPropertyName());
-//        property.setAddress(propertyDetails.getAddress());
-//        property.setCity(propertyDetails.getCity());
-//        property.setPincode(propertyDetails.getPincode());
-//        property.setBuilding(propertyDetails.getBuilding());
-//        property.setFloorNumber(propertyDetails.getFloorNumber());
-//        property.setFlatNumber(propertyDetails.getFlatNumber());
-//       // property.setDocument(propertyDetails.getDocument());
-//
-//        return propertyRepository.save(property);
-//    }
-//
-//    public void deleteProperty(int id) {
-//        Property property = getPropertyById(id);
-//        propertyRepository.delete(property);
-//    }
-//    @ResponseStatus(value = HttpStatus.NOT_FOUND)
-//    public class ResourceNotFoundException extends RuntimeException {
-//        public ResourceNotFoundException(String resourceName, String fieldName, int fieldValue) {
-//            super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
-//        }
-//    }
-//}
+package com.document_management.Service;
+        import com.document_management.Entity.Property;
+        import com.document_management.Repository.PropertyRepository;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.http.HttpStatus;
+        import org.springframework.stereotype.Service;
+        import org.springframework.web.bind.annotation.ResponseStatus;
+
+        import java.util.List;
+
+@Service
+public class PropertyService {
+    @Autowired
+    private PropertyRepository propertyRepository;
+
+    public List<Property> getAllProperties() {
+        return propertyRepository.findAll();
+    }
+
+    public Property getPropertyById(int id) {
+        return propertyRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Property", "id", id));
+    }
+
+    public Property createProperty(Property property) {
+        return propertyRepository.save(property);
+    }
+
+    public Property updateProperty(int id, Property propertyDetails) {
+        Property property = getPropertyById(id);
+
+        property.setPropertyName(propertyDetails.getPropertyName());
+        property.setAddress(propertyDetails.getAddress());
+        property.setCity(propertyDetails.getCity());
+        property.setPincode(propertyDetails.getPincode());
+        property.setBuilding(propertyDetails.getBuilding());
+        property.setFloorNumber(propertyDetails.getFloorNumber());
+        property.setFlatNumber(propertyDetails.getFlatNumber());
+       // property.setDocument(propertyDetails.getDocument());
+
+        return propertyRepository.save(property);
+    }
+
+    public void deleteProperty(int id) {
+        Property property = getPropertyById(id);
+        propertyRepository.delete(property);
+    }
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    public class ResourceNotFoundException extends RuntimeException {
+        public ResourceNotFoundException(String resourceName, String fieldName, int fieldValue) {
+            super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
+        }
+    }
+}
