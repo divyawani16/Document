3e1c37a3d55f79cbe98c814e16bde892c4a01078
divya wani <divyaw@bbd.co.za>
2023-04-11 10:53:33 +0530

    changes

--- src/main/java/com/document_management/swaggerConfig/SwaggerConfig.java
+++ src/main/java/com/document_management/swaggerConfig/SwaggerConfig.java
@@ -1,9 +1,6 @@
 package com.document_management.swaggerConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -11,27 +8,16 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
-@EnableWebMvc
+
 public class SwaggerConfig {
     @Bean
-    public Docket SwaggerConfig(){
+    public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .paths(PathSelectors.any())
-                .apis(RequestHandlerSelectors.basePackage("com.document_management"))
+                .apis(RequestHandlerSelectors.any())
                 .build();
     }
-    @Bean
-    public WebMvcConfigurer webMvcConfigurer()
-    {
-        return new WebMvcConfigurer()
-        {
-            @Override
-            public void addResourceHandlers( ResourceHandlerRegistry registry )
-            {
-                registry.addResourceHandler( "swagger-ui.html" ).addResourceLocations( "classpath:/META-INF/resources/" );
-                registry.addResourceHandler( "/webjars/**" ).addResourceLocations( "classpath:/META-INF/resources/webjars/" );
-            }
-        };
-    }
+
+
 }
