a876c06f384eb42bdf6d7e61eda66c1a7e436688
divya wani <divyaw@bbd.co.za>
2023-04-14 12:24:02 +0530

    changes

---
+++ src/main/java/com/document_management/DTO/DocMimeTypeDto.java
@@ -0,0 +1,9 @@
+package com.document_management.DTO;
+
+import lombok.Data;
+@Data
+public class DocMimeTypeDto {
+
+        private Integer docMimeTypeId;
+        private String docMimeTypeName;
+}
