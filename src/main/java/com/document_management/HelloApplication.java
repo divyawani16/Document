b5584b51530fab5286974d97a61c41b32666135e
divya wani <divyaw@bbd.co.za>
2023-04-21 17:21:27 +0530

    changes

--- src/main/java/com/document_management/HelloApplication.java
+++ src/main/java/com/document_management/HelloApplication.java
@@ -1,7 +1,9 @@
 package com.document_management;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -13,5 +15,9 @@ public class HelloApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
 	}
+	@Bean
 
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 }
