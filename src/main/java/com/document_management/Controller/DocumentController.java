7dcc2aa2654f28cb80d0a6fcc4bca1aa919f4cde
divyawani16 <divyaw@bbd.co.za>
2023-06-06 10:23:43 +0530

    download added with documentid

--- src/main/java/com/document_management/Controller/DocumentController.java
+++ src/main/java/com/document_management/Controller/DocumentController.java
@@ -3,13 +3,10 @@ import com.document_management.AWSS3Service;
 import com.document_management.DTO.DocumentDetailsDto;
 import com.document_management.DTO.DocumentDto;
 import com.document_management.Entity.*;
-import com.document_management.FileService;
 import com.document_management.Repository.*;
 import com.document_management.Service.DocumentService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,12 +16,11 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
 
 @RestController
 @RequestMapping("api/documents")
 
-public class DocumentController {
+    public class DocumentController {
     private final DocumentService documentService;
     private UsersRepository usersRepository;
     private PropertyRepository propertyRepository;
@@ -50,30 +46,28 @@ public class DocumentController {
     }
 
     @GetMapping("/count")
+    @CrossOrigin(origins = "http://localhost:4200")
     public ResponseEntity<Integer> countDocument() {
         int count = documentService.getAllDocuments().size();
         return ResponseEntity.ok(count);
     }
+
     @GetMapping("/{documentId}/download")
+    @CrossOrigin(origins = "http://localhost:4200")
+
     public ResponseEntity<Resource> downloadDocument(@PathVariable Integer documentId) throws IOException {
         return documentService.downloadDocument(documentId);
     }
 
-//    @GetMapping("/{documentId}/download")
-//    public ResponseEntity<Resource> downloadDocument(@PathVariable Integer documentId) throws IOException {
-//        Resource fileResource = documentService.downloadDocument(documentId);
-//        return ResponseEntity.ok()
-//                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileResource.getFilename() + "\"")
-//                .body(fileResource);
-//    }
-
 
     @GetMapping("/documentsdetails")
+    @CrossOrigin(origins = "http://localhost:4200")
     public List<DocumentDetailsDto> getAllDocumentsWithDetails() {
         List<Document> documents = documentRepository.findAll();
         List<DocumentDetailsDto> documentDetails = new ArrayList<>();
         for (Document document : documents) {
             DocumentDetailsDto details = new DocumentDetailsDto();
+            details.setDocumentId(document.getDocumentId());
             details.setDocumentName(document.getDocumentName());
             Users user = document.getUser();
             if (user != null) {
@@ -91,22 +85,48 @@ public class DocumentController {
             if (docMimeType != null) {
                 details.setDocMimeTypeName(docMimeType.getDocMimeTypeName());
             }
-            documentDetails.add(details);
+                documentDetails.add(details);
         }
         return documentDetails;
     }
 
-    @GetMapping("/documents/propertyname")
-    public List<Document> searchDocumentsByPropertyName(@RequestParam("propertyName") String propertyName) {
-        return documentRepository.findByPropertyPropertyName(propertyName);
+//    @GetMapping("/documents/propertyname")
+//    @CrossOrigin(origins = "http://localhost:4200")
+//    public List<Document> searchDocumentsByPropertyName(@RequestParam("propertyName") String propertyName) {
+//        return documentRepository.findByPropertyPropertyName(propertyName);
+//    }
+@GetMapping("/documents/propertyname")
+@CrossOrigin(origins = "http://localhost:4200")
+public List<DocumentDetailsDto> searchDocumentsByPropertyName(@RequestParam("propertyName") String propertyName) {
+    List<Document> documents = documentRepository.findByPropertyPropertyName(propertyName);
+    return convertToDocumentDetailsDto(documents);
+}
+
+    private List<DocumentDetailsDto> convertToDocumentDetailsDto(List<Document> documents) {
+        List<DocumentDetailsDto> dtos = new ArrayList<>();
+        for (Document document : documents) {
+            DocumentDetailsDto dto = new DocumentDetailsDto();
+            dto.setDocumentId(document.getDocumentId());
+            dto.setDocumentName(document.getDocumentName());
+            dto.setUserName(document.getUser().getUsername());
+            dto.setPropertyName(document.getProperty().getPropertyName());
+            dto.setDocTypeName(document.getDocType().getDocTypeName());
+            dto.setDocMimeTypeName(document.getDocMimeType().getDocMimeTypeName());
+            dtos.add(dto);
+        }
+        return dtos;
     }
 
+
+
     @GetMapping("/documents/username")
+    @CrossOrigin(origins = "http://localhost:4200")
     public List<Document> searchDocumentsByUsername(@RequestParam("username") String username) {
         return documentService.searchDocumentsByUsername(username);
     }
 
     @PostMapping
+    @CrossOrigin(origins = "http://localhost:4200")
     public ResponseEntity<String> addDocument(
             @RequestParam("file") MultipartFile file,
             @RequestParam("documentName") String documentName,
@@ -143,12 +163,14 @@ public class DocumentController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteDocument(@PathVariable Integer id) {
-        documentService.deleteDocument(id);
+    @CrossOrigin(origins = "http://localhost:4200")
+    public ResponseEntity<Void> deleteDocument(@PathVariable Integer documentId) {
+        documentService.deleteDocument(documentId);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/get")
+    @CrossOrigin(origins = "http://localhost:4200")
     public ResponseEntity<List<DocumentDto>> getAllDocuments() {
         List<Document> documents = documentService.getAllDocuments();
         List<DocumentDto> documentDtos = documents.stream()
@@ -158,57 +180,52 @@ public class DocumentController {
     }
 
     @GetMapping("/{documentId}")
+    @CrossOrigin(origins = "http://localhost:4200")
     public ResponseEntity<Document> getDocumentById(@PathVariable Integer documentId) {
         Document document = documentService.getDocumentById(documentId);
         return ResponseEntity.ok(document);
     }
+
+    @PutMapping("/{documentId}")
+    @CrossOrigin(origins = "http://localhost:4200")
+    public ResponseEntity<String> updateDocument(
+            @PathVariable Integer documentId,
+            @RequestParam("file") MultipartFile file,
+            @RequestParam("documentName") String documentName,
+            @RequestParam("username") String username,
+            @RequestParam("propertyName") String propertyName,
+            @RequestParam("docTypeName") String docTypeName,
+            @RequestParam("docMimeTypeName") String docMimeTypeName
+    ) {
+        Document document = documentService.getDocumentById(documentId);
+
+        if (document == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        document.setDocumentName(documentName);
+
+        Users user = usersRepository.findByUsername(username)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid username"));
+        document.setUser(user);
+
+        Property property = propertyRepository.findByPropertyName(propertyName)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid propertyName"));
+        document.setProperty(property);
+
+        DocType docType = docTypeRepository.findByDocTypeName(docTypeName)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid docTypeName"));
+        document.setDocType(docType);
+
+        Optional<DocMimeType> optionalDocMimeType = docMimeTypeRepository.findByDocMimeTypeName(docMimeTypeName);
+        DocMimeType docMimeType = optionalDocMimeType.orElseThrow(() -> new IllegalArgumentException("Invalid docMimeTypeName"));
+        document.setDocMimeType(docMimeType);
+
+        String publicURL = awsS3Service.uploadFile(file);
+        document.setFilePath(publicURL);
+
+        documentService.addDocument(document);
+
+        return ResponseEntity.ok("Document updated successfully.");
+    }
 }
-    //    @PostMapping
-//    public ResponseEntity<String> addDocument(
-//            @RequestParam("file") MultipartFile file,
-//            @RequestParam("documentName") String documentName,
-//            @RequestParam("username") String username,
-//            @RequestParam("propertyName") String propertyName,
-//            @RequestParam("docTypeName") String docTypeName,
-//            @RequestParam("docMimeTypeName") String docMimeTypeName
-//    ) {
-//        try {
-//            Document document = new Document();
-//            document.setDocumentName(documentName);
-//
-//            Users user = usersRepository.findByUsername(username)
-//                    .orElseThrow(() -> new IllegalArgumentException("Invalid username"));
-//            document.setUser(user);
-//
-//            Property property = propertyRepository.findByPropertyName(propertyName)
-//                    .orElseThrow(() -> new IllegalArgumentException("Invalid propertyName"));
-//            document.setProperty(property);
-//
-//            DocType docType = docTypeRepository.findByDocTypeName(docTypeName)
-//                    .orElseThrow(() -> new IllegalArgumentException("Invalid docTypeName"));
-//            document.setDocType(docType);
-//
-//            Optional<DocMimeType> optionalDocMimeType = docMimeTypeRepository.findByDocMimeTypeName(docMimeTypeName);
-//            DocMimeType docMimeType = optionalDocMimeType.orElseThrow(() -> new IllegalArgumentException("Invalid docMimeTypeName"));
-//            document.setDocMimeType(docMimeType);
-//
-//            String storagePath = documentService.saveFile(file);
-//            document.setFilePath(storagePath);
-//
-//            documentService.addDocument(document);
-//
-//            return ResponseEntity.ok("Document added successfully.");
-//        } catch (IOException e) {
-//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to add document.");
-//        }
-//    }
-
-
-//    @GetMapping("/{documentId}/download")
-//    public ResponseEntity<Resource> downloadDocument(@PathVariable Integer documentId) throws IOException {
-//        Resource fileResource = documentService.downloadDocument(documentId);
-//
-//        return ResponseEntity.ok()
-//                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileResource.getFilename() + "\"")
-//                .body(fileResource);
-//    }
--- src/main/java/com/document_management/Service/DocumentService.java
+++ src/main/java/com/document_management/Service/DocumentService.java
@@ -8,10 +8,7 @@ import com.document_management.AWSS3Service;
 import com.document_management.DTO.DocumentDetailsDto;
 import com.document_management.DTO.DocumentDto;
 import com.document_management.Entity.*;
-import com.document_management.Repository.DocMimeTypeRepository;
-import com.document_management.Repository.DocTypeRepository;
-import com.document_management.Repository.DocumentRepository;
-import com.document_management.Repository.PropertyRepository;
+import com.document_management.Repository.*;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
@@ -22,6 +19,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
@@ -43,7 +42,8 @@ public class DocumentService {
     @Autowired
     private DocMimeTypeRepository docMimeTypeRepository;
     private final DocumentRepository documentRepository;
-
+@Autowired
+private UsersRepository usersRepository;
     @Autowired
     private final ModelMapper modelMapper;
     private final AWSS3Service awsS3Service;
@@ -64,7 +64,7 @@ public class DocumentService {
         S3ObjectInputStream inputStream = s3Object.getObjectContent();
         InputStreamResource inputStreamResource = new InputStreamResource(inputStream);
 
-        // Extract the filename from the file path
+
         String filename = extractFilename(document.getFilePath());
 
         HttpHeaders headers = new HttpHeaders();
@@ -77,36 +77,50 @@ public class DocumentService {
     }
 
     private String extractFilename(String filePath) {
-        // Use string manipulation to extract the filename
         String[] pathParts = filePath.split("/");
         return pathParts[pathParts.length - 1];
     }
 
+    public ResponseEntity<String> updateDocument(
+            Integer documentId,
+            MultipartFile file,
+            String documentName,
+            String username,
+            String propertyName,
+            String docTypeName,
+            String docMimeTypeName
+    ) throws IOException {
+        Document document = getDocumentById(documentId);
 
+        if (document == null) {
+            return ResponseEntity.notFound().build();
+        }
 
+        document.setDocumentName(documentName);
 
-//    private String getFilenameFromPath(String filePath) {
-//        Path path = Paths.get(filePath);
-//        return path.getFileName().toString();
-//    }
+        Users user = usersRepository.findByUsername(username)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid username"));
+        document.setUser(user);
 
-//    public Resource downloadDocument(Integer documentId) throws IOException {
-//        Document document = documentRepository.findById(documentId)
-//                .orElseThrow(() -> new NotFoundException("Document not found"));
-//
-//        AmazonS3 s3Client = awsS3Service.getAmazonS3Client();
-//        System.out.println(document.getFilePath());
-//        S3Object s3Object = s3Client.getObject("documentsmanagement", document.getFilePath());
-//
-//        S3ObjectInputStream inputStream = s3Object.getObjectContent();
-//        InputStreamResource inputStreamResource = new InputStreamResource(inputStream);
-//
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentDispositionFormData("attachment", document.getFilePath());
-//        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-//        return inputStreamResource;
-//    }
+        Property property = propertyRepository.findByPropertyName(propertyName)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid propertyName"));
+        document.setProperty(property);
 
+        DocType docType = docTypeRepository.findByDocTypeName(docTypeName)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid docTypeName"));
+        document.setDocType(docType);
+
+        Optional<DocMimeType> optionalDocMimeType = docMimeTypeRepository.findByDocMimeTypeName(docMimeTypeName);
+        DocMimeType docMimeType = optionalDocMimeType.orElseThrow(() -> new IllegalArgumentException("Invalid docMimeTypeName"));
+        document.setDocMimeType(docMimeType);
+
+        String publicURL = awsS3Service.uploadFile(file);
+        document.setFilePath(publicURL);
+
+        addDocument(document);
+
+        return ResponseEntity.ok("Document updated successfully.");
+    }
     public List<Document> getAllDocuments() {
         return documentRepository.findAll();
     }
@@ -133,53 +147,25 @@ public class DocumentService {
 
         return publicURL;
     }
-    public List<Document> searchDocumentsByPropertyName(String propertyName) {
-        return documentRepository.findByPropertyPropertyName(propertyName);
+//    public List<Document> searchDocumentsByPropertyName(String propertyName) {
+//        return documentRepository.findByPropertyPropertyName(propertyName);
+//    }
+public List<Document> searchDocumentsByPropertyName(String propertyName) {
+    List<Document> documents = documentRepository.findByPropertyPropertyName(propertyName);
+    List<Document> smartworksDocuments = new ArrayList<>();
+
+    for (Document document : documents) {
+        if (document.getProperty().getPropertyName().equals("Smartworks")) {
+            smartworksDocuments.add(document);
+        }
     }
+
+    return smartworksDocuments;
+}
 
     public List<Document> searchDocumentsByUsername(String username) {
         return documentRepository.findByUserUsername(username);
     }
-//    public String saveFile(MultipartFile file) throws IOException {
-//        String storagePath = "D://xyz"; // Specify the desired storage path here
-//
-//        // Create the necessary directories if they don't exist
-//        File storageDir = new File(storagePath);
-//        if (!storageDir.exists()) {
-//            storageDir.mkdirs();
-//        }
-//
-//        // Construct the file path by concatenating the storage path and the original file name
-//        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
-//        String filePath = storagePath + File.separator + fileName;
-//
-//        // Transfer the file to the target location
-//        File targetFile = new File(filePath);
-//        file.transferTo(targetFile);
-//
-//        return filePath;
-//    }
-
-//    public String saveFile(MultipartFile file) throws IOException {
-//        String storagePath = "D://xyz"; // Specify the desired storage path here
-//
-//        // Create the necessary directories if they don't exist
-//        File storageDir = new File(storagePath);
-//        if (!storageDir.exists()) {
-//            storageDir.mkdirs();
-//        }
-//
-//        // Construct the file path by concatenating the storage path and the original file name
-//        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
-//        String filePath = storagePath + File.separator + fileName;
-//
-//        // Transfer the file to the target location
-//        File targetFile = new File(filePath);
-//        file.transferTo(targetFile);
-//
-//        return filePath;
-//    }
-
     public DocMimeType getDocMimeTypeById(Integer docMimeTypeId) {
         return docMimeTypeRepository.findById(docMimeTypeId)
                 .orElseThrow(() -> new NotFoundException("DocMimeType not found"));
@@ -216,11 +202,6 @@ public class DocumentService {
                 .orElseThrow(() -> new RuntimeException("Document not found"));
         return modelMapper.map(document, Document.class);
     }
-//    public DocumentDto getDocumentById(Integer documentId) {
-//        Document document = documentRepository.findById(documentId)
-//                .orElseThrow(() -> new RuntimeException("Document not found"));
-//        return modelMapper.map(document, DocumentDto.class);
-//    }
 
     public Optional<Property> getPropertyByName(String propertyName) {
         return propertyRepository.findByPropertyName(propertyName);
@@ -233,15 +214,5 @@ public class DocumentService {
     public Optional<DocMimeType> getDocMimeTypeByName(String docMimeTypeName) {
         return docMimeTypeRepository.findByDocMimeTypeName(docMimeTypeName);
     }
-//    public Resource downloadDocument(Integer documentId) {
-//        Document document = documentRepository.findById(documentId)
-//                .orElseThrow(() -> new NotFoundException("Document not found"));
-//
-//        File file = new File(document.getFilePath());
-//        if (!file.exists()) {
-//            throw new NotFoundException("File not found");
-//        }
-//
-//        return new FileSystemResource(file);
-//    }
+
 }
