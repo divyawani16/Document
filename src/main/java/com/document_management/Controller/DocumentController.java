7ed6d2a5c93bbf735486842ec69ded4c219e4dc6
AnujaNikumbh5 <anuja@bbd.co.za>
2023-06-09 09:23:36 +0530

    access for admin role

--- src/main/java/com/document_management/Controller/DocumentController.java
+++ src/main/java/com/document_management/Controller/DocumentController.java
@@ -8,6 +8,7 @@ import com.document_management.Service.DocumentService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
@@ -61,6 +62,7 @@ public class DocumentController {
 
 
     @GetMapping("/documentsdetails")
+    @PreAuthorize("hasRole('Admin')")
     @CrossOrigin(origins = "http://localhost:4200")
     public List<DocumentDetailsDto> getAllDocumentsWithDetails() {
         List<Document> documents = documentRepository.findAll();
